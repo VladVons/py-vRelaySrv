77ecc13fc855c38c980ecf49882f8baca2d432de
VladVons <vladvons@gmail.com>
2021-03-09 15:32:21 +0200

    MyCommit

--- src/py-vRelaySrv.py
+++ src/py-vRelaySrv.py
@@ -15,32 +15,40 @@ from Inc.Plugin import Plugin
 #
 #import App
 
+class TApp():
+    def InitLog(self):
+        _, Name = os.path.split(__file__)
+        FileLog = '/var/log/py-vRelaySrv/%s.log' % (Name)
+        try:
+            with open(FileLog, 'a') as File:
+                File.writable()
+        except:
+            FileLog = '%s.log' % (Name)
+        Log.AddEcho(TEchoFile(FileLog))
+        print('Log file ' + FileLog)
 
-async def Run():
-    _, Name = os.path.split(__file__)
-    FileLog = '/var/log/py-vRelaySrv/%s.log' % (Name)
-    Log.AddEcho(TEchoFile(FileLog))
-    print('Log file ' + FileLog)
+    async def Run(self):
+        self.InitLog()
+        Log.Print(1, 'i', 'Run', os.uname())
 
-    Log.Print(1, 'i', 'Run', os.uname())
-
-    Plugin.LoadList(Conf.get('Plugins'))
-    try:
-        await Plugin.Run()
-    except KeyboardInterrupt:
-        Log.Print(1, 'x', 'Run()', 'Ctrl-C')
-    finally:
-        await Plugin.Stop()
+        Plugin.LoadList(Conf.get('Plugins'))
+        try:
+            await Plugin.Run()
+        except KeyboardInterrupt:
+            Log.Print(1, 'x', 'Run()', 'Ctrl-C')
+        finally:
+            await Plugin.Stop()
 
 
+Task = TApp().Run()
 if (Conf.Debug):
     event_loop = asyncio.get_event_loop()
     event_loop.set_debug(True)
     event_loop.slow_callback_duration = 0.001
-    event_loop.run_until_complete(Run())
+    event_loop.run_until_complete(Task)
 else:
     #event_loop = asyncio.get_event_loop()
-    #event_loop.run_until_complete(Run())
+    #event_loop.run_until_complete(Task)
 
     # >= 3.7
-    asyncio.run(Run())
+    asyncio.run(Task)
--- src/py-vRelaySrv.sh
+++ src/py-vRelaySrv.sh
@@ -3,3 +3,6 @@
 
 #/usr/bin/env python3 py-vRelaySrv.py
 /usr/bin/env python3 -B py-vRelaySrv.py
+
+#pip3.7 install gmqtt aiomysql --user
+/usr/bin/env python3.7 -B py-vRelaySrv.py
