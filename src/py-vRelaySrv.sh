e459152d306c7cb9825cdf455218d7982f13940c
VladVons <vladvons@gmail.com>
2021-12-18 10:16:50 +0200

    MyCommit

--- src/py-vRelaySrv.sh
+++ src/py-vRelaySrv.sh
@@ -2,7 +2,7 @@
 #--- VladVons@gmail.com
 
 #/usr/bin/env python3 py-vRelaySrv.py
-/usr/bin/env python3 -B py-vRelaySrv.py
+#/usr/bin/env python3 -B py-vRelaySrv.py
 
 #pip3.7 install aiohtt gmqtt aiomysql --user
 /usr/bin/env python3.7 -B py-vRelaySrv.py
