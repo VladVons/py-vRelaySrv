5f5361db8bfa9e5d7d1bf0833085d99beb9fc10b
vladvons <vladvons@gmail.com>
2022-10-15 12:17:09 +0300

    MyCommit

--- src/Inc/UtilP/UFS.py
+++ src/Inc/UtilP/UFS.py
@@ -10,11 +10,16 @@ import re
 
 
 def GetFiles(aPath: str, aMask: str = '.*', aDepth: int = 99):
+    '''
+    iterator function
+    for x in GetFiles('/etc', '.bin$')
+    '''
     if (os.path.exists(aPath)):
         for File in sorted(os.listdir(aPath)):
             Path = aPath + '/' + File
-            if (os.path.isfile(Path)):
+            if (os.path.isdir(Path)) and (not os.path.islink(Path)):
+                if (aDepth >= 0):
+                    yield from GetFiles(Path, aMask, aDepth - 1)
+            else:
                 if (re.search(aMask, File)):
                     yield Path
-            elif (aDepth >= 0):
-                yield from GetFiles(Path, aMask, aDepth - 1)
