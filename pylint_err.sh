b932d054637bc0c2818b72f7f0ad9a27b7aeda89
vladvons <vladvons@gmail.com>
2022-10-16 19:32:15 +0300

    MyCommit

--- pylint_err.sh
+++ pylint_err.sh
@@ -4,4 +4,4 @@ File="pylint_err.log"
 Ignore="C0103|C0114|C0115|C0116|C0209|C0325|C0301|R0903|R0914"
 
 echo "Creating $File ..."
-pylint --recursive=y ./src | egrep -v $Ignore > $File
+time pylint --recursive=y ./src | egrep -v $Ignore > $File
