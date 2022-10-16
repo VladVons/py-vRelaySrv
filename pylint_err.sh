89b8f974b5aa66d0d9543e4f906c0604bd041144
vladvons <vladvons@gmail.com>
2022-10-16 13:13:09 +0300

    MyCommit

--- pylint_err.sh
+++ pylint_err.sh
@@ -1,3 +1,3 @@
 # pip3 install pylint
 Ignore="C0103|C0114|C0115|C0116|C0209|C0325|C0301|R0903|R0914"
-pylint --recursive=y . | egrep -v $Ignore > pylint_err.txt
+pylint --recursive=y ./src | egrep -v $Ignore > pylint_err.txt
