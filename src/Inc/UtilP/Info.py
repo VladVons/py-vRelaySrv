87c06182052a36b07856eb7551a859f9c6bf1bc4
vladvons <vladvons@gmail.com>
2022-10-24 13:21:35 +0300

    MyCommit

--- src/Inc/UtilP/Info.py
+++ src/Inc/UtilP/Info.py
@@ -7,16 +7,22 @@ import os
 import sys
 import time
 import platform
+#
+from Inc.UtilP.Time import SecondsToDHMS
 
 
 def GetSysInfo() -> dict:
+    Uptime = SecondsToDHMS(time.monotonic())
+    Uptime = [f'{Val} {Key}' for Key, Val in Uptime.items()]
+
     UName =  platform.uname()
     Res = {
-        'App' : os.path.basename(sys.argv[0]).rsplit('.')[0],
+        'App': os.path.basename(sys.argv[0]).rsplit('.')[0],
         'OS': UName.system,
         'Host': UName.node,
         'User': os.environ.get('USER'),
         'Python': (sys.version_info.major, sys.version_info.minor, sys.version_info.micro),
+        'Uptime': ', '.join(Uptime),
         'Now': time.strftime('%Y-%m-%d %H:%M:%S')
     }
     return Res
--- src/Inc/UtilP/Time.py
+++ src/Inc/UtilP/Time.py
@@ -16,3 +16,28 @@ class TASleep():
         self._Cnt += 1
         if (self._Cnt % self.Cnt == 0):
             await asyncio.sleep(self.Sleep)
+
+intervals = (
+    ('weeks', 604800),  # 60 * 60 * 24 * 7
+    ('days', 86400),    # 60 * 60 * 24
+    ('hours', 3600),    # 60 * 60
+    ('minutes', 60),
+    ('seconds', 1),
+)
+
+
+def SecondsToDHMS(aSeconds: int) -> dict:
+    Measure = (
+        ('days', 60 * 60 * 24),
+        ('hours', 60 * 60),
+        ('minutes', 60),
+        ('seconds', 1)
+    )
+
+    Res = {}
+    for Key, Val in Measure:
+        Value = int(aSeconds / Val)
+        if (Value):
+            aSeconds -= Value * Val
+            Res[Key] = Value
+    return Res
