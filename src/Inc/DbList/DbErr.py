cb96051da147d0b46d1dab121344afe92adea532
VladVons <vladvons@gmail.com>
2023-03-10 22:18:47 +0200

    MyCommit

--- src/Inc/DbList/DbErr.py
+++
@@ -1,7 +0,0 @@
-# Created: 2022.03.24
-# Author: Vladimir Vons <VladVons@gmail.com>
-# License: GNU, see LICENSE for more details
-
-
-class TDbListException(Exception):
-    pass
--- src/Inc/SrvWeb/__init__.py
+++
@@ -1,8 +0,0 @@
-# Created: 2023.03.01
-# Author: Vladimir Vons <VladVons@gmail.com>
-# License: GNU, see LICENSE for more details
-# pip3 install aiopg
-
-
-from .SrvBase import TSrvBase, TSrvConf
-from .Common import FileReader
