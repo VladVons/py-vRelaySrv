d12af3978bdc33214387819aed114910d4187f3b
vladvons <vladvons@gmail.com>
2023-07-22 14:31:50 +0300

    MyCommit

--- src/Task/WebSrv/www/js/TDbListUse.js
+++ src/Task/WebSrv/www/js/TDbListUse.js
@@ -1,14 +1,14 @@
-const {TDbList, TDbRec}  = require('./TDbList');
+const {TDbList, TDbRec}  = require('./TDbList')
 
 Data1 = '{ \
     "head": ["user", "age", "male"],\
     "data": [\
-        ["user2", 22, true],\
-        ["user1", 11, false],\
+        ["user4", 44, true],\
         ["user3", 33, true],\
-        ["user4", 44, true]\
+        ["user2", 22, true],\
+        ["user1", 11, false]\
     ]\
-}';
+}'
 
 //const Data2 = JSON.parse(Data1)
 //const Dbl = new TDbList(Data2)
@@ -16,9 +16,8 @@ const Dbl = new TDbList().ImportStr(Data1)
 console.log('GetSize', Dbl.GetSize())
 console.log('Rec.AsDict', Dbl.Rec.GetAsDict())
 
-Dbl.Sort('user')
 for (let Rec of Dbl) {
-    console.log(Rec.GetField('user'));
+    console.log(Rec.GetField('user'))
 }
 
 Dbl.RecAdd(['user5', 33, false])
@@ -27,12 +26,16 @@ Dbl.RecAdd().SetAsDict({'user': 'John', 'age': 55, 'male': true})
 
 Rec = Dbl.RecAdd()
 Rec.SetField('user', 'pink')
+Rec.SetField('age', 41)
 
 console.log('')
 console.log('GetSize', Dbl.GetSize())
+Dbl.Sort('user')
 for (let Rec of Dbl) {
-    console.log(Rec.Data);
+    console.log(Rec.Data)
 }
 
+console.log('')
+console.log('Export')
 Data = Dbl.Export()
 console.log(Data)
